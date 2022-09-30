0ffcfe2d250eb168053bc8691bf115e4ebf5e6c1
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-09-30 16:49:11 +0200

    Objectif 1 atteint
    Vérification de test où deux "DumbIA" jouent 3 coups chacune.

--- connect4/dumb_ia.py
+++ connect4/dumb_ia.py
@@ -1,6 +1,16 @@
 from .game import Grid, Player
-
+from .game import Cell #Ajout de la classe Cell
 
 class DumbIA(Player):
     def play(self, grid: Grid) -> int:
-        ...
+        i=0 # indice de boucle
+        j=0 #indice de boucle
+        # parcours de la grille
+        for i in range(grid.lines) :
+            for j in range(grid.columns) :
+                if grid.grid[i][j]==Cell.EMPTY : #Test de case vide
+                    return j #retourne l indice la colonne ou le jeton a ete place et quitte
+
+
+
+

323f45d3ebf8687c48f576d69cc030c07422e14e
gbrl-damien <112943682+gbrl-damien@users.noreply.github.com>
2022-10-05 11:32:37 +0200

    Objectif 5
    Implémentation de l'IA Cheater_B et vérification du test.

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -1,8 +1,13 @@
-from .game import Grid, Player
+from .game import Grid, Player, Cell
 
 
 class CheaterB(Player):
     """This IA cheats and modify the grid to ensure player B wins."""
 
     def play(self, grid: Grid) -> int:
-        ...
+        #on remplace la grille actuelle par un grille gagnante pour B 
+        
+        for j in range(1,grid.columns-1) : #rempli toutes les colones de la premiere ligne
+            grid.place(j,Cell.B) #le joueur B joue 'O'          
+        
+        return 6
